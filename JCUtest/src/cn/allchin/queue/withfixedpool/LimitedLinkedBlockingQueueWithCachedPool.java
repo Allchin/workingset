6d64c4374d0ed026093f5610baf4a33c9a4d85b6
renxing.zhang <renxing.zhang@qunar.com>
2018-12-28 20:20:05 +0800

    +名称区分limit unlimit blocking qurere

---
+++ JCUtest/src/cn/allchin/queue/withfixedpool/LimitedLinkedBlockingQueueWithCachedPool.java
@@ -0,0 +1,59 @@
+package cn.allchin.queue.withfixedpool;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import cn.allchin.queue.QueueThreadPoolCreater;
+
+/**
+ *
+ active|0|max|4|core|2|inqueue|-3|queueSize|0|queueRemain|3
+active|1|max|4|core|2|inqueue|-3|queueSize|0|queueRemain|3
+active|1|max|4|core|2|inqueue|-3|queueSize|0|queueRemain|3
+active|2|max|4|core|2|inqueue|-3|queueSize|0|queueRemain|3
+active|2|max|4|core|2|inqueue|-1|queueSize|1|queueRemain|2
+active|2|max|4|core|2|inqueue|-1|queueSize|1|queueRemain|2
+active|2|max|4|core|2|inqueue|-1|queueSize|1|queueRemain|2
+active|2|max|4|core|2|inqueue|1|queueSize|2|queueRemain|1
+active|2|max|4|core|2|inqueue|1|queueSize|2|queueRemain|1
+active|2|max|4|core|2|inqueue|3|queueSize|3|queueRemain|0
+active|2|max|4|core|2|inqueue|3|queueSize|3|queueRemain|0
+active|3|max|4|core|2|inqueue|3|queueSize|3|queueRemain|0
+active|3|max|4|core|2|inqueue|3|queueSize|3|queueRemain|0
+active|4|max|4|core|2|inqueue|3|queueSize|3|queueRemain|0
+Exception in thread "main" java.util.concurrent.RejectedExecutionException: Task Thread[Thread-8,5,main] rejected from java.util.concurrent.ThreadPoolExecutor@33909752[Running, pool size = 4, active threads = 4, queued tasks = 3, completed tasks = 0]
+	at java.util.concurrent.ThreadPoolExecutor$AbortPolicy.rejectedExecution(ThreadPoolExecutor.java:2047)
+	at java.util.concurrent.ThreadPoolExecutor.reject(ThreadPoolExecutor.java:823)
+	at java.util.concurrent.ThreadPoolExecutor.execute(ThreadPoolExecutor.java:1369)
+	at cn.allchin.queue.PoolRunner.addJobToPool(PoolRunner.java:49)
+	at cn.allchin.queue.PoolRunner.testOne(PoolRunner.java:39)
+	at cn.allchin.queue.PoolRunner.main(PoolRunner.java:43)
+active|4|max|4|core|2|inqueue|3|queueSize|3|queueRemain|0
+active|4|max|4|core|2|inqueue|3|queueSize|3|queueRemain|0
+active|4|max|4|core|2|inqueue|3|queueSize|3|queueRemain|0
+active|4|max|4|core|2|inqueue|3|queueSize|3|queueRemain|0
+active|4|max|4|core|2|inqueue|3|queueSize|3|queueRemain|0
+active|4|max|4|core|2|inqueue|3|queueSize|3|queueRemain|0
+由ticker退出系统
+
+
+
+
+ * 
+ * @author renxing.zhang
+ *
+ */
+public class LimitedLinkedBlockingQueueWithCachedPool implements QueueThreadPoolCreater {
+
+	int nThreads = 2;
+	ThreadPoolExecutor pool = new ThreadPoolExecutor(nThreads, nThreads+2, 0L, TimeUnit.MILLISECONDS,
+			new LinkedBlockingQueue<Runnable>(3));
+	//设置了linkedBlocking的size后，效果和ArrayBlockingQueueWithCachedPool 效果一样了
+
+	@Override
+	public ThreadPoolExecutor createPool() {
+		return pool;
+	}
+}
